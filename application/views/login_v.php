bff3ec00951b140bbad44dd157cd6bef8a5d9907
bagasdewandana <muhammaddewandana@gmail.com>
2022-02-03 08:25:35 +0700

    pengaduan

---
+++ application/views/login_v.php
@@ -0,0 +1,57 @@
+<div class="header">
+  <h2 style="background-color: #01579b; padding: 2%; color: white;">Login</h2>
+</div>
+<div class="container">
+
+  <!-- Outer Row -->
+  <div class="row justify-content-center">
+
+    <div class="col-lg-6">
+
+      <div class="card o-hidden border-0 shadow-lg my-5">
+        <div class="card-body p-0">
+          <!-- Nested Row within Card Body -->
+          <div class="row">
+            <div class="col-lg-12">
+              <div class="p-5">
+                <div class="text-center">
+                  <img src="\pengaduan-masyarakat\assets\backend\img\logo1.png" alt="sipansi" style="width:150px; height: 150px;">
+                  <h1 class="h4 text-gray-900 mb-4" style="padding-top: 8%;">Selamat Datang di Aplikasi Pelaporan Absensi Rusak</h1>
+                  
+                  
+                </div>
+
+                <?= validation_errors('<div class="alert alert-danger alert-dismissible fade show" role="alert">','<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                </div>') ?>
+
+                <?= $this->session->flashdata('msg'); ?>
+                
+                <?= form_open('Auth/LoginController', 'class="user"'); ?>
+                  <div class="form-group">
+                    <input type="text" class="form-control form-control-user" id="username" name="username" placeholder="Username" autofocus value="<?= set_value('username') ?>">
+                  </div>
+                  <div class="form-group">
+                    <input type="password" class="form-control form-control-user" id="password" name="password" placeholder="Password">
+                  </div>
+                  <button type="submit" class="btn btn-primary btn-user btn-block">Login</button>
+                </form>
+                <hr>
+                <div class="text-center">
+                  <a class="small" href="<?= base_url('Auth/RegisterController') ?>">Register?</a>
+                </div>
+                <div class="text-center" style="padding-top: 8%;">
+                  <img src="\pengaduan-masyarakat\assets\backend\img\kominfo.png" alt="kominfo" style="width:105px; height: 80px;">
+                  <img src="\pengaduan-masyarakat\assets\backend\img\logo2.png" alt="lamongan" style="width:60px; height: 70px;">
+                  <img src="\pengaduan-masyarakat\assets\backend\img\logo3.png" alt="megilan" style="width:165px; height: 50px; padding-left: 5%;">
+              </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+  </div>
+
+</div>
---
+++ vendor/dompdf/dompdf/lib/fonts/dompdf_font_family_cache.php
@@ -0,0 +1,98 @@
+<?php return array (
+  'sans-serif' => array(
+    'normal' => $fontDir . '/Helvetica',
+    'bold' => $fontDir . '/Helvetica-Bold',
+    'italic' => $fontDir . '/Helvetica-Oblique',
+    'bold_italic' => $fontDir . '/Helvetica-BoldOblique',
+  ),
+  'times' => array(
+    'normal' => $fontDir . '/Times-Roman',
+    'bold' => $fontDir . '/Times-Bold',
+    'italic' => $fontDir . '/Times-Italic',
+    'bold_italic' => $fontDir . '/Times-BoldItalic',
+  ),
+  'times-roman' => array(
+    'normal' => $fontDir . '/Times-Roman',
+    'bold' => $fontDir . '/Times-Bold',
+    'italic' => $fontDir . '/Times-Italic',
+    'bold_italic' => $fontDir . '/Times-BoldItalic',
+  ),
+  'courier' => array(
+    'normal' => $fontDir . '/Courier',
+    'bold' => $fontDir . '/Courier-Bold',
+    'italic' => $fontDir . '/Courier-Oblique',
+    'bold_italic' => $fontDir . '/Courier-BoldOblique',
+  ),
+  'helvetica' => array(
+    'normal' => $fontDir . '/Helvetica',
+    'bold' => $fontDir . '/Helvetica-Bold',
+    'italic' => $fontDir . '/Helvetica-Oblique',
+    'bold_italic' => $fontDir . '/Helvetica-BoldOblique',
+  ),
+  'zapfdingbats' => array(
+    'normal' => $fontDir . '/ZapfDingbats',
+    'bold' => $fontDir . '/ZapfDingbats',
+    'italic' => $fontDir . '/ZapfDingbats',
+    'bold_italic' => $fontDir . '/ZapfDingbats',
+  ),
+  'symbol' => array(
+    'normal' => $fontDir . '/Symbol',
+    'bold' => $fontDir . '/Symbol',
+    'italic' => $fontDir . '/Symbol',
+    'bold_italic' => $fontDir . '/Symbol',
+  ),
+  'serif' => array(
+    'normal' => $fontDir . '/Times-Roman',
+    'bold' => $fontDir . '/Times-Bold',
+    'italic' => $fontDir . '/Times-Italic',
+    'bold_italic' => $fontDir . '/Times-BoldItalic',
+  ),
+  'monospace' => array(
+    'normal' => $fontDir . '/Courier',
+    'bold' => $fontDir . '/Courier-Bold',
+    'italic' => $fontDir . '/Courier-Oblique',
+    'bold_italic' => $fontDir . '/Courier-BoldOblique',
+  ),
+  'fixed' => array(
+    'normal' => $fontDir . '/Courier',
+    'bold' => $fontDir . '/Courier-Bold',
+    'italic' => $fontDir . '/Courier-Oblique',
+    'bold_italic' => $fontDir . '/Courier-BoldOblique',
+  ),
+  'dejavu sans' => array(
+    'bold' => $fontDir . '/DejaVuSans-Bold',
+    'bold_italic' => $fontDir . '/DejaVuSans-BoldOblique',
+    'italic' => $fontDir . '/DejaVuSans-Oblique',
+    'normal' => $fontDir . '/DejaVuSans',
+  ),
+  'dejavu sans mono' => array(
+    'bold' => $fontDir . '/DejaVuSansMono-Bold',
+    'bold_italic' => $fontDir . '/DejaVuSansMono-BoldOblique',
+    'italic' => $fontDir . '/DejaVuSansMono-Oblique',
+    'normal' => $fontDir . '/DejaVuSansMono',
+  ),
+  'dejavu serif' => array(
+    'bold' => $fontDir . '/DejaVuSerif-Bold',
+    'bold_italic' => $fontDir . '/DejaVuSerif-BoldItalic',
+    'italic' => $fontDir . '/DejaVuSerif-Italic',
+    'normal' => $fontDir . '/DejaVuSerif',
+  ),
+  'font awesome 5 brands' => array(
+    'normal' => $fontDir . '/font-awesome-5-brands-normal_c54f8356a21137f24f04342fc492c000',
+  ),
+  'font awesome 5 free' => array(
+    'normal' => $fontDir . '/font-awesome-5-free-normal_873908ac572cb414de916da0a0ad02c1',
+    '900' => $fontDir . '/font-awesome-5-free-900_657a0c2b0eada88f4d4978e264897207',
+  ),
+  'lora' => array(
+    'bold' => $fontDir . '/lora-bold_5bf57f1a54b91ab1c676b1d697183261',
+  ),
+  'montserrat' => array(
+    '200' => $fontDir . '/montserrat-200_8a676a97be7a09e72b1baa44bc447884',
+    'normal' => $fontDir . '/montserrat-normal_031f963ec8fabef52ee680059a10dca4',
+    '600' => $fontDir . '/montserrat-600_70c50087dd3358c70d57c3efda7ef573',
+  ),
+  'pacifico' => array(
+    'normal' => $fontDir . '/pacifico-normal_5a830e267628b38832a5321d32619233',
+  ),
+) ?>
